cf35c9cc83d5361052482e5826c467f4405b6562
EliasAlas03 <aa0452032023@unab.edu.sv>
2024-10-11 09:58:49 -0600

    Segunda Guia Dapper

---
+++ DapperDemo_Programacion2-f41b2f72b9705716343f5ca94d60409f4b9da757/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace DapperDemo_Programacion2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvCustomers = new System.Windows.Forms.DataGridView();
+            this.btnObtenerTodos = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvCustomers
+            // 
+            this.dgvCustomers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCustomers.Location = new System.Drawing.Point(12, 28);
+            this.dgvCustomers.Name = "dgvCustomers";
+            this.dgvCustomers.RowHeadersWidth = 51;
+            this.dgvCustomers.RowTemplate.Height = 24;
+            this.dgvCustomers.Size = new System.Drawing.Size(994, 361);
+            this.dgvCustomers.TabIndex = 0;
+            // 
+            // btnObtenerTodos
+            // 
+            this.btnObtenerTodos.Location = new System.Drawing.Point(360, 417);
+            this.btnObtenerTodos.Name = "btnObtenerTodos";
+            this.btnObtenerTodos.Size = new System.Drawing.Size(103, 51);
+            this.btnObtenerTodos.TabIndex = 1;
+            this.btnObtenerTodos.Text = "Obtener Todos";
+            this.btnObtenerTodos.UseVisualStyleBackColor = true;
+            this.btnObtenerTodos.Click += new System.EventHandler(this.btnObtenerTodos_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1018, 541);
+            this.Controls.Add(this.btnObtenerTodos);
+            this.Controls.Add(this.dgvCustomers);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvCustomers;
+        private System.Windows.Forms.Button btnObtenerTodos;
+    }
+}
+
---
+++ DapperDemo_Programacion2-f41b2f72b9705716343f5ca94d60409f4b9da757/Form1.cs
@@ -0,0 +1,36 @@
+﻿using AccesoDatos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DapperDemo_Programacion2
+{
+    public partial class Form1 : Form
+    {
+        CustomerRepository customerR = new CustomerRepository();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnObtenerTodos_Click(object sender, EventArgs e)
+        {
+            var cliente = customerR.ObtenerTodo();
+            dgvCustomers.DataSource = cliente;
+        }
+
+        
+    }
+}
